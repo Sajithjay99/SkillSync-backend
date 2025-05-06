ac626488547290dabcbfde3ca445ed73af18e8df
Lasitha Ravimal <lasitharavimal7@gmail.com>
2025-05-06 23:11:06 +0530

    refactor: Enhance SignupDTO class with Lombok annotations for cleaner code

--- src/main/java/com/springbootacademy/skill_sync/dto/SignupDTO.java
+++ src/main/java/com/springbootacademy/skill_sync/dto/SignupDTO.java
@@ -1,5 +1,11 @@
 package com.springbootacademy.skill_sync.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupDTO {
-    
+    private String username;
+    private String password;
 }
