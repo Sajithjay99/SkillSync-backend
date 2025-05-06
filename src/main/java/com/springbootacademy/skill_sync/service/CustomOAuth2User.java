24f13d1b2c45c7101e4173b0e8ed1ddcd5f2c80f
Lasitha Ravimal <lasitharavimal7@gmail.com>
2025-05-06 23:21:48 +0530

    feat: Implement CustomOAuth2User class for OAuth2 user handling

---
+++ src/main/java/com/springbootacademy/skill_sync/service/CustomOAuth2User.java
@@ -0,0 +1,12 @@
+package com.springbootacademy.skill_sync.service;
+
+import com.example.pafbackend.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+    private final OAuth2User oauth2User;
+    private final User user;
