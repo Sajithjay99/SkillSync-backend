61eae5f96114692a3c5e613a9242b6c3969ec370
Lasitha Ravimal <lasitharavimal7@gmail.com>
2025-05-07 13:48:27 +0530

    Add Learning entity class for managing learning entries

---
+++ src/main/java/com/springbootacademy/skill_sync/entity/Learning.java
@@ -0,0 +1,30 @@
+package com.springbootacademy.skill_sync.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Document(collection = "learning")
+public class Learning {
+    @Id
+    private String id;
+    private String userId;
+    private String topic;
+    private String description;
+    private String resourceLink;
+    private String status;
+    private LocalDateTime timestamp;
+    private String reflection;
+    private String template;
+
+    // Template-specific fields (remaining)
+    private String certificationName;   // For "Certification/Qualification"
+    private String provider;            // For "Certification/Qualification" and "Workshops/Bootcamps"
+    private String dateObtained;        // For "Certification/Qualification"
+
+    
