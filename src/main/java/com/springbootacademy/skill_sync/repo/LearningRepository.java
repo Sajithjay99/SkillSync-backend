3727b88f7984730866debde5c3ec8db88a3d05dc
Lasitha Ravimal <lasitharavimal7@gmail.com>
2025-05-07 15:03:21 +0530

    Add LearningRepository class for managing Learning entities

---
+++ src/main/java/com/springbootacademy/skill_sync/repo/LearningRepository.java
@@ -0,0 +1,12 @@
+package com.springbootacademy.skill_sync.repo;
+
+import com.example.pafbackend.models.Learning;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+public class LearningRepository {
+    
+}
