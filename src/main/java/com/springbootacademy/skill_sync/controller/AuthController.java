986fbc7be8c34dbb4e0994749f1967c46f7df7ea
Lasitha Ravimal <lasitharavimal7@gmail.com>
2025-05-07 08:44:59 +0530

    feat: Implement AuthController with initial structure and imports

---
+++ src/main/java/com/springbootacademy/skill_sync/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.springbootacademy.skill_sync.controller;
+
+
+
+import com.example.pafbackend.config.TokenGenerator;
+import com.example.pafbackend.dto.LoginDTO;
+import com.example.pafbackend.dto.SignupDTO;
+import com.example.pafbackend.dto.TokenDTO;
+import com.example.pafbackend.dto.UserDTO;
+import com.example.pafbackend.models.User;
+import com.example.pafbackend.services.CustomOAuth2User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AuthController {
+    
+}
