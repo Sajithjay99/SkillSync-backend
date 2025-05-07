0c9a427e1aaab35938be1589f73b799be75e190a
Sajithjay99 <sajithjay99@gmail.com>
2025-05-07 10:27:14 +0530

    Enhance TokenGenerator to include refresh token creation and validation logic

--- src/main/java/com/springbootacademy/skill_sync/config/TokenGenerator.java
+++ src/main/java/com/springbootacademy/skill_sync/config/TokenGenerator.java
@@ -49,3 +49,47 @@ public class TokenGenerator {
            throw err;
        }
     }
+    private String createRefreshToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Instant now = Instant.now();
+
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .issuer("myApp")
+                .issuedAt(now)
+                .expiresAt(now.plus(30, ChronoUnit.DAYS))
+                .subject(user.getId())
+                .build();
+
+        return refreshTokenEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+    }
+
+    public TokenDTO createToken(Authentication authentication) {
+        if (!(authentication.getPrincipal() instanceof User user)) {
+            throw new BadCredentialsException(
+                    MessageFormat.format("principal {0} is not of User type", authentication.getPrincipal().getClass())
+            );
+        }
+
+        TokenDTO tokenDTO = new TokenDTO();
+        tokenDTO.setUserId(user.getId());
+        tokenDTO.setAccessToken(createAccessToken(authentication));
+
+        String refreshToken;
+        if (authentication.getCredentials() instanceof Jwt jwt) {
+            Instant now = Instant.now();
+            Instant expiresAt = jwt.getExpiresAt();
+            Duration duration = Duration.between(now, expiresAt);
+            long daysUntilExpired = duration.toDays();
+            if (daysUntilExpired < 7) {
+                refreshToken = createRefreshToken(authentication);
+            } else {
+                refreshToken = jwt.getTokenValue();
+            }
+        } else {
+            refreshToken = createRefreshToken(authentication);
+        }
+        tokenDTO.setRefreshToken(refreshToken);
+
+        return tokenDTO;
+    }
+}
