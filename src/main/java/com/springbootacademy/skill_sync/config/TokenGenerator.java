2e3657c7b571d2fb0d6e454f48482613843093cb
Sajithjay99 <sajithjay99@gmail.com>
2025-05-07 10:26:47 +0530

    feat: Implement TokenGenerator for JWT access token creation

---
+++ src/main/java/com/springbootacademy/skill_sync/config/TokenGenerator.java
@@ -0,0 +1,51 @@
+package com.example.pafbackend.config;
+
+import com.example.pafbackend.models.User;
+import com.example.pafbackend.dto.TokenDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Component
+public class TokenGenerator {
+    private static final Logger logger = LoggerFactory.getLogger(TokenGenerator.class);
+    @Autowired
+    JwtEncoder accessTokenEncoder;
+
+    @Autowired
+    @Qualifier("jwtRefreshTokenEncoder")
+    JwtEncoder refreshTokenEncoder;
+
+    private String createAccessToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Instant now = Instant.now();
+
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .issuer("myApp")
+                .issuedAt(now)
+                .expiresAt(now.plus(5, ChronoUnit.DAYS))
+                .subject(user.getId())
+                .build();
+
+       try{
+           return accessTokenEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+       }catch (Exception err){
+           logger.error("Error Generating Access Token: {}", err.getMessage(), err);
+           throw err;
+       }
+    }
